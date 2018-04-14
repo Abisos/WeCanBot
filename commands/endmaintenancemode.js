d1d2e4a914584e3f46ed55e97ede9c64fb6604c9
Jonah Kraushaar <jonah.kraushaar@gmx.net>
2018-04-14 18:04:35 +0200

    style updates

--- commands/endmaintenancemode.js
+++ commands/endmaintenancemode.js
@@ -1,25 +1,25 @@
 module.exports = {
-    name: 'endmaintenancemode',
-    description: 'Ends the maintenancemode.',
-    aliases:["emtm","endmtm"],
-    usage: '[praefix+commandname]',
-    cooldown:60,
-    guildOnly:true,
-    execute(message, args, Perms) {
-      const fs = require('fs');
+  name: 'endmaintenancemode',
+  description: 'Ends the maintenancemode.',
+  aliases: ["emtm", "endmtm"],
+  usage: '[praefix+commandname]',
+  cooldown: 60,
+  guildOnly: true,
+  execute(message, args, Perms) {
+    const fs = require('fs');
 
-      const Permissionsraw = fs.readFileSync('./Perms.json');
-    	const Permissions = JSON.parse(Permissionsraw);
+    const Permissionsraw = fs.readFileSync('./Perms.json');
+    const Permissions = JSON.parse(Permissionsraw);
 
-      if (Permissions[message.guild.id].maintenancemodebool) {
-        Permissions[message.guild.id].maintenancemodebool = false;
-        message.channel.send("The Maintenancemode was ended.").then(msg => msg.delete(9000));
-      }else {
-        message.channel.send("There is no maintenancemode active.").then(msg => msg.delete(9000));
-      }
-      const returnPerms = JSON.stringify(Permissions, null, 2);
-      fs.writeFileSync('./Perms.json',returnPerms);
-      message.delete(9000);
+    if (Permissions[message.guild.id].maintenancemodebool) {
+      Permissions[message.guild.id].maintenancemodebool = false;
+      message.channel.send("The Maintenancemode was ended.").then(msg => msg.delete(9000));
+    } else {
+      message.channel.send("There is no maintenancemode active.").then(msg => msg.delete(9000));
+    }
+    const returnPerms = JSON.stringify(Permissions, null, 2);
+    fs.writeFileSync('./Perms.json', returnPerms);
+    message.delete(9000);
 
-    },
-  };
+  },
+};
--- commands/stats.js
+++ commands/stats.js
@@ -1,25 +1,25 @@
 module.exports = {
-    name: 'stats',
-    description: 'Returns the users discordtag.',
-    aliases:["ss","serverinfo","server"],
-    usage: '[praefix+commandname]',
-    cooldown:4,
-    guildOnly:true,
-    execute(message, args, client) {
-            const discord = require('discord.js');
+  name: 'stats',
+  description: 'Returns the users discordtag.',
+  aliases: ["ss", "serverinfo", "server"],
+  usage: '[praefix+commandname]',
+  cooldown: 4,
+  guildOnly: true,
+  execute(message, args, client) {
+    const discord = require('discord.js');
 
-            let embed = new discord.RichEmbed().setAuthor(`${message.guild.name}`)
-                            .setColor("#4286f4")
-                            .setThumbnail(message.guild.iconURL)
-                            .addField("Owner", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator} - ${message.guild.owner.user.presence.status}`)
-                            .addField("Server createdAt", message.guild.createdAt.toString().slice(0,33),true)
-                            .addField("Total Members", message.guild.memberCount, true)
-                            .addField("Online", message.guild.members.filterArray(m => m.presence.status === 'online'||m.presence.status === 'idle').length, true)
-                            .addField("Offline", message.guild.members.filterArray(m => m.presence.status === 'offline'||m.presence.status === 'dnd').length, true);
+    let embed = new discord.RichEmbed().setAuthor(`${message.guild.name}`)
+      .setColor("#4286f4")
+      .setThumbnail(message.guild.iconURL)
+      .addField("Owner", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator} - ${message.guild.owner.user.presence.status}`)
+      .addField("Server createdAt", message.guild.createdAt.toString().slice(0, 33), true)
+      .addField("Total Members", message.guild.memberCount, true)
+      .addField("Online", message.guild.members.filterArray(m => m.presence.status === 'online' || m.presence.status === 'idle').length, true)
+      .addField("Offline", message.guild.members.filterArray(m => m.presence.status === 'offline' || m.presence.status === 'dnd').length, true);
 
-                          //
-            message.delete(60000);
-            message.channel.send(embed).then(msg => msg.delete(60000));;
+    //
+    message.delete(60000);
+    message.channel.send(embed).then(msg => msg.delete(60000));;
 
-    },
-  };
+  },
+};
