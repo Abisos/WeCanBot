ca648e55e60797a9e1f7e7a252dba585929ea304
Abisos <38558001+Abisos@users.noreply.github.com>
2018-06-22 14:05:47 +0200

    Reworked the embed message to show the game name and title of the stream.

--- util.js
+++ util.js
@@ -1,5 +1,10 @@
 module.exports = {
-  // returns the display name of the given user from given guild
+  /**
+   * returns the display name of the given user from given guild
+   * @param {Guild} [guild] The guild in which to fetch the member
+   * @param {Snowflake} user The member for which to get the display name
+   * @returns {string} The display name of a member
+   */
   getDisplayName: function(guild, user) {
     return new Promise(function (resolve,  reject) {
       guild.fetchMember(user)
@@ -11,4 +16,94 @@ module.exports = {
         });
     });
   },
+
+  /**
+   * returns a json result from twitch api for the current stream from a user
+   * @param {string} twitchlink The stream link for the request
+   * @returns {string} A json formattet string with data about the stream
+   */
+  getTwitchStream: function(twitchlink) {
+    var request = require('request');
+    const config = require('./config.json');
+
+    // get username from link
+    const regex = /(https:\/\/www\.twitch\.tv\/)(.+)/gm;
+    let m;
+    var username;
+
+    while ((m = regex.exec(twitchlink)) != null) { // necessary to avoid infinite loops with zero-width matches
+      if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+      }
+
+      username = m[2];
+    }
+
+    // call twitch api
+    var headers = {
+      'Client-ID': config.twitchClientID
+    };
+
+    var options = {
+      url: `https://api.twitch.tv/helix/streams?user_login=${username}`,
+      headers: headers
+    };
+
+    return new Promise(function(resolve, reject) {
+      function callback(error, response, body) {
+        if (error) return reject(error);
+
+        if (!error && response.statusCode == 200) {
+          var data = JSON.parse(body);
+
+          if (data.data[0] && data.data[0].type === "live") {
+            data.data[0].twitchname = username;
+            return resolve(data.data[0]);
+          } else {
+            return reject();
+          }
+        }
+      }
+
+      request(options, callback);
+    });
+  },
+
+  /**
+   * returns a json result from twitch api for the id of a game
+   * @param {string} gameID The game id for the request
+   * @returns {string} The name of the game
+   */
+  getTwitchGame: function(gameID) {
+    var request = require('request');
+    const config = require('./config.json');
+
+    // call twitch api
+    var headers = {
+      'Client-ID': config.twitchClientID
+    };
+
+    var options = {
+      url: `https://api.twitch.tv/helix/games?id=${gameID}`,
+      headers: headers
+    };
+
+    return new Promise(function(resolve, reject) {
+      function callback(error, response, body) {
+        if (error) return reject(error);
+
+        if (!error && response.statusCode == 200) {
+          var data = JSON.parse(body);
+
+          if (data.data[0]) {
+            return resolve(data.data[0].name);
+          } else {
+            return resolve("unknown game")
+          }
+        }
+      }
+
+      request(options, callback);
+    });
+  }
 }
